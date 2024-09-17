153d5b0846cd90281e9581a7efeed2e7708f3b10
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-09-17 14:20:20 +0100

    Resolve merge conflit - updateItems.js

--- api/controllers/products/productDetailTable/updateItems.js
+++ api/controllers/products/productDetailTable/updateItems.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Module to update a single product detail in the 'product_detail' table in a PostgreSQL database
 // This module provides functionality to update one product detail using the 'item_id'.
 // The function can update either the size or price of the product detail.
@@ -69,68 +68,3 @@ const updateItem = async (item) => {
  */
 // Export the function for use in other parts of the application
 module.exports = updateItem;
-=======
-// Module to update product details in the 'product_detail' table in a PostgreSQL database
-// This module provides functionality to update multiple product details using the 'item_id'.
-// Each item can update the size and price of the product details.
-
-// Import the database connection module
-const db = require('../../database/postgress');
-
-/**
- * Asynchronous function to update an array of items in the 'product_detail' table.
- * 
- * @param {Array<Object>} items - Array of items to be updated. Each object should contain:
- *   - item_id: The unique identifier of the product detail to be updated (integer).
- *   - size: The new size of the product (optional, string with a max length of 50).
- *   - price: The new price of the product (optional, numeric value).
- * 
- * @returns {Promise<void>} - A promise that resolves when all items are updated.
- * @throws {Error} - Throws an error if the update operation fails.
- */
-const updateItems = async (items) => {
-    try {
-        // Start a transaction
-        await db.query('BEGIN');
-
-        // Iterate over each item and perform an update based on the item_id
-        for (const item of items) {
-            const { item_id, size, price } = item;
-
-            // Construct the query based on provided fields (size, price)
-            let updateQuery = 'UPDATE product_detail SET ';
-            const updateParams = [];
-            let paramIndex = 1;
-
-            if (size) {
-                updateQuery += `size = $${paramIndex++}, `;
-                updateParams.push(size);
-            }
-
-            if (price) {
-                updateQuery += `price = $${paramIndex++}, `;
-                updateParams.push(price);
-            }
-
-            // Remove trailing comma and space, then add WHERE clause
-            updateQuery = updateQuery.slice(0, -2) + ` WHERE item_id = $${paramIndex}`;
-            updateParams.push(item_id);
-
-            // Execute the query to update the item
-            await db.query(updateQuery, updateParams);
-        }
-
-        // Commit the transaction if all updates are successful
-        await db.query('COMMIT');
-        console.log('Items updated successfully.');
-    } catch (err) {
-        // Rollback the transaction in case of any error
-        await db.query('ROLLBACK');
-        console.error('Error updating items:', err);
-        throw err;
-    }
-};
-
-// Export the function for use in other parts of the application
-module.exports = updateItems;
->>>>>>> ab3ec94ed277df8fe2d002be8685cd435aa30604
--- api/controllers/users/index.js
+++ api/controllers/users/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const createUserAccount = require('./createUserAccount')
 const createDatabase = require('../database/createDatabase')
 const createUserTable = require('./createUserTable')
@@ -16,19 +15,3 @@ module.exports = {
     userLogin,
 };
 
-=======
-const createUserAccount = require('./createUserAccount')
-const createDatabase = require('../database/createDatabase')
-const createUserTable = require('./createUserTable')
-const userLogin = require('./userLogin')
-
-// Re-export all modules in a single object
-// this makes my code look organized
-
-module.exports = {
-    createUserAccount,
-    createDatabase,
-    createUserTable,
-    userLogin,
-};
->>>>>>> ab3ec94ed277df8fe2d002be8685cd435aa30604
