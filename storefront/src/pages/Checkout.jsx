7824544a3281ba28b29f3b990fdc30d45ddf7d45
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-09-22 18:00:35 +0100

    update checkout page, add cart folder

--- storefront/src/pages/Checkout.jsx
+++ storefront/src/pages/Checkout.jsx
@@ -1,33 +1,60 @@
 import React from 'react'
-import { useOutletContext } from 'react-router-dom'
+import { Link, useOutletContext } from 'react-router-dom'
 
 function Checkout() {
     const [siteData] = useOutletContext()
     const { cart } = siteData
     console.log(cart)
-    return (
+    return (<section className='mt-6'>
+        <div className='flex justify-between m-6'>
+            <p className='p-4'>
+                SHOP &#187; CHECKOUT
+            </p>
+            <Link to='/' className='mr-10 bg-slate-500 text-slate-100 p-4'>
+                Continue shopping &#187;
+            </Link>
+        </div>
         <div className='lg:flex justify-between lg:w-[85%] m-auto'>
-            <div className='w-[59%] cart-details rounded-md p-4'>
+            <div className='w-[59%] cart-details rounded-md p-4 bg-slate-200 divide-y divide-slate-400'>
+                <p className='text-left p-4 text-lg font-extrabold'>
+                    MY CART DETAILS
+                </p>
                 {cart.length === 0 && <div>EMPTY CART</div>}
-                {cart.length > 0 && cart.map(item => <div className='flex'>
-                    <p className='w-[100px] p-2'>
-                        {<img src={item.product_image_url} alt="" />}
+                {cart.length > 0 && cart.map(item => <div key={item.product_id} className='flex justify-between pt-2'>
+                    <p className='w-[100px] p-2 m-2'>
+                        {< img src={item.product_image_url} alt="" />}
 
                     </p>
 
                     <p className='text-2xl'>
-                        {item.product_name} <br />
+                        <strong> {item.product_name} </strong> <br />
                         {item.size} <br />
-                        {item.price}
+                        &#8358;{item.price}
                     </p>
 
-                </div>)}
+                    <p className='fgrid place-items-center'>
+                        <span>&minus;</span>
+                        <input className='w-[30px] h-[30px] justify-end' type="text" defaultValue={0} />
+                        <span> &#x2B; </span>
+                    </p>
+
+                    <p className='text-2xl font-extrabold'>
+                        &times;
+                    </p>
+
+                </div>)
+                }
+            </div >
+
+            <div className='w-[39%]'>
+                <p className='lg:bg-red-100 payment-proccessor rounded-md p-4 bg-slate-300'>
+
+                    Mike Payment
+                </p>
             </div>
 
-            <div className='w-[39%] payment-proccessor rounded-md p-4'>
-                payment proccessor here
-            </div>
-        </div>
+        </div >
+    </section >
     )
 }
 
