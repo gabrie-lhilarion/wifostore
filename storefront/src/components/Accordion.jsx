814050d7004e12c4d08d07048408d50ee9df33c1
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-10-01 10:05:40 +0100

    Refactor: move accordion in checkout page to components/accordion

--- storefront/src/components/Accordion.jsx
+++ storefront/src/components/Accordion.jsx
@@ -21,8 +21,6 @@ function Accordion({ setPayment, cart, setSiteData }) {
     return (
         <div className='lg:w-[59%] cart-details rounded-md bg-slate-200 rounded-tl-sm rounded-tr-sm'>
 
-
-
             <div className='accordion divide-y divide-slate-400 rounded-tl-sm rounded-tr-sm'>
                 <div className='accordion-item rounded-tl-sm rounded-tr-sm'>
                     <h1 className='accordion-header p-3 bg-slate-200 rounded-tl-sm rounded-tr-sm'>
