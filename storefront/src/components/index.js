d5cf15cb627d3eca1c8a4fe5180f0d24bbe72f7c
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-10-02 06:16:51 +0100

    Fix: merge conflict

--- storefront/src/components/index.js
+++ storefront/src/components/index.js
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-
-export { default as Overlay } from './Overlay'
-export { default as User } from './User'
-export { default as Login } from './Login'
-export { default as SignIn } from './SignIn'
-export { default as SideNavigation } from './SideNavigation'
-export { default as Logo } from './Logo'
-export { default as UserAndCart } from './UserAndCart'
-export { default as SearchProducts } from './SearchProducts'
-export { default as Footer } from './Footer'
-export { default as Mansonry } from './Mansonry'
-export { default as MobileShoppingCart } from './MobileShoppingCart'
-export { default as PriceList } from './PriceList'
-export { default as Item } from './Item'
-export { default as QuantityControl } from './QuantityControl'
-export { default as StickyMobileHeader } from './StickyMobileHeader'
-export { default as Accordion } from './Accordion'
-=======
 export { default as Overlay } from './Overlay'
 export { default as User } from './User'
 export { default as Login } from './Login'
@@ -36,4 +17,3 @@ export { default as Accordion } from './Accordion'
 export { default as StepOne } from './StepOne'
 export { default as StepTwo } from './StepTwo'
 export { default as StepThree } from './StepThree'
->>>>>>> gabriel_shopping_experience
